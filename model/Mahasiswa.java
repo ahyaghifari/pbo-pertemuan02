31c1296ce83739764948ee2845ec1480c5f57774
ahyaghifari <ahyaghifari288@gmail.com>
2023-04-05 11:57:39 +0800

    Memanggil method dari banyak Class dari package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+   public static void tampilanInfo(){
+	System.out.println();
+	System.out.println("== Data Mahasiswa =======================");
+	System.out.println("NPM : 2110010456");
+	System.out.println("Nama : Muhammad Ahya Ghifari");
+	System.out.println("=========================================");
+}
+}
